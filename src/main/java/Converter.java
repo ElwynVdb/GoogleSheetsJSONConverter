6f86e1d481d475aacf2de68251b22ed484fcc835
Josia50 <elwyn.vdb@gmail.com>
2020-01-16 22:20:23 +0100

    Changes & Checks

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -20,10 +20,10 @@ public class Converter {
     public static Gson GSON = new Gson().newBuilder().setPrettyPrinting().create();
 
     public static void main(String[] args) {
-        String id = JOptionPane.showInputDialog(null, "Enter SheetID", "Sheets to JSON", JOptionPane.OK_OPTION);
+        String spreadsheetID = JOptionPane.showInputDialog(null, "Enter SpreadSheetID", "Spreadsheet to JSON", JOptionPane.OK_OPTION);
 
-        if (id != null)
-            System.out.println(convert(Utils.getJsonFromURL("https://spreadsheets.google.com/feeds/cells/" + id + "/1/public/full?alt=json")));
+        if (spreadsheetID != null)
+            System.out.println(convert(Utils.getJsonFromURL("https://spreadsheets.google.com/feeds/cells/" + spreadsheetID + "/1/public/full?alt=json")));
         else
             System.out.println("Please provide a valid SheetID");
     }
@@ -37,37 +37,36 @@ public class Converter {
     public static String convert(String json) {
         if (json != null) {
             try {
-                SpreadSheet sheet = GSON.fromJson(json, SpreadSheet.class);
-                List<String> fields = getFieldNames(sheet);
-                int checkvalue = (sheet.feed.entry[sheet.feed.entry.length - 1].gs$cell.row) * fields.size();
+                SpreadSheet spreadSheet = GSON.fromJson(json, SpreadSheet.class);
+                List<String> keys = getKeys(spreadSheet);
+                int checkvalue = (spreadSheet.feed.entry[spreadSheet.feed.entry.length - 1].gs$cell.row) * keys.size();
 
-                if (checkvalue > sheet.feed.entry.length) {
-                    sheet = fixEmptyFields(fields.size(), sheet, sheet.feed.entry[sheet.feed.entry.length - 1].gs$cell.row);
+                if (checkvalue > spreadSheet.feed.entry.length) {
+                    spreadSheet = fixEmptyFields(keys.size(), spreadSheet, spreadSheet.feed.entry[spreadSheet.feed.entry.length - 1].gs$cell.row);
                 }
 
-                List<SpreadSheet.Entry> entries = Arrays.asList(sheet.feed.entry);
-                JSONArray object = new JSONArray();
+                List<SpreadSheet.Entry> entries = Arrays.asList(spreadSheet.feed.entry);
+                JSONArray array = new JSONArray();
 
-                for (int x = 0; x < (sheet.feed.entry.length / fields.size()); x++) {
+                for (int x = 0; x < (spreadSheet.feed.entry.length / keys.size()); x++) {
                     JSONObject jsonObject = new JSONObject();
 
                     if (x == 0) continue;
 
-                    for (int i = 0; i < fields.size(); i++) {
-                        int selection = (x * fields.size()) + i;
-                        String field = fields.get(i);
+                    for (int i = 0; i < keys.size(); i++) {
+                        int selection = (x * keys.size()) + i;
+                        String key = keys.get(i);
 
-                        jsonObject.put(field, entries.get(selection).gs$cell.$t);
+                        jsonObject.put(key, entries.get(selection).gs$cell.$t);
                     }
 
-                    object.put(jsonObject);
+                    array.put(jsonObject);
                 }
 
-                return object.toString();
+                return array.toString();
 
             } catch (JsonSyntaxException e) {
             } catch (JSONException e) {
-
             }
         }
 
@@ -80,49 +79,50 @@ public class Converter {
      * @param sheet
      * @return List<String>
      */
-    public static List<String> getFieldNames(SpreadSheet sheet) {
-        List<String> fields = new ArrayList<>();
+    public static List<String> getKeys(SpreadSheet sheet) {
+        List<String> keys = new ArrayList<>();
 
         for (SpreadSheet.Entry entry : sheet.feed.entry) {
 
             if (entry.gs$cell.row > 1) break;
 
-            fields.add(entry.gs$cell.$t);
+            keys.add(entry.gs$cell.$t);
         }
 
-        return fields;
+        return keys;
     }
 
     /**
      * Fix any empty fields (Prevent JSON writing to mess up)
      *
      * @param fieldAmount
-     * @param sheet
+     * @param spreadSheet
      * @param rowsMax
      * @return
      */
-    public static SpreadSheet fixEmptyFields(int fieldAmount, SpreadSheet sheet, int rowsMax) {
+    public static SpreadSheet fixEmptyFields(int fieldAmount, SpreadSheet spreadSheet, int rowsMax) {
+
+        int max = fieldAmount * rowsMax;
+        List<Integer> validValues = new ArrayList<>();
+        SpreadSheet.Entry[] entries = spreadSheet.feed.entry;
 
         SpreadSheet newsheet = new SpreadSheet();
-        SpreadSheet.Entry[] entries = sheet.feed.entry;
-        int max = fieldAmount * rowsMax;
-
         newsheet.feed.entry = new SpreadSheet.Entry[max];
         SpreadSheet.Entry[] newEntry = newsheet.feed.entry;
-        List<Integer> passed = new ArrayList<>();
 
-        // Initialize all fields
+
+        // Initialize all values
         for (int a = 0; a < max; a++) {
             newEntry[a] = new SpreadSheet.Entry();
             newEntry[a].gs$cell = new SpreadSheet.Cell();
         }
 
-        // Put everything into new Array at their respected location
+        // Put everything into new Array at their required location
         for (int x = 0; x < entries.length; x++) {
 
             SpreadSheet.Entry entry = entries[x];
             int selection = ((entry.gs$cell.row - 1) * fieldAmount) + (entry.gs$cell.col - 1);
-            passed.add(selection);
+            validValues.add(selection);
             SpreadSheet.Entry entry1 = newEntry[selection];
 
             entry1.gs$cell.col = entry.gs$cell.col;
@@ -130,10 +130,10 @@ public class Converter {
             entry1.gs$cell.$t = entry.gs$cell.$t;
         }
 
-        // Fill in the empty fields in array with "Empty"
+        // Fill in the empty values in array with "Empty"
         for (int i = 0; i < max; i++) {
 
-            if (!passed.contains(i)) {
+            if (!validValues.contains(i)) {
                 SpreadSheet.Entry entry1 = newEntry[i];
                 entry1.gs$cell.col = (i - (i / fieldAmount * fieldAmount)) + 1;
                 entry1.gs$cell.row = i / fieldAmount;
